7278cdaafe83f1df736a8ab01c8d5dd31b1b7c13
Adhil Novandri <novandri.adhil@sysmex.co.id>
2022-02-02 17:01:11 +0700

    update home introduction

--- resources/views/urin/home.blade.php
+++ resources/views/urin/home.blade.php
@@ -2,19 +2,53 @@
 
 @section('content')
 
-<p>Selamat datang di website EQUAS Urin</p>
-<p>Terima kasih telah menggunakan layanan website unggah hasil EQUAS.</p>
+<p>Pelanggan Yth.</p>
 <p>
-    Jika terdapat pertanyaan, silakan hubungi kami melalui email di Sysmex.Urinalysis@sysmex.co.id
-    atau telepon ke Sysmex Call Center di (021) 3002 6999 / (021) 2902 3008.
+    Kami mengundang Anda untuk berpartisipasi dalam Sysmex EQUAS U-FCM Periode 2021. 
+    EQUAS adalah sebuah kombinasi program IQC (Internal Quality Control), 
+    EQAS (External Quality Assessment Scheme) dan Servis Teknis, 
+    yang diselenggarakan oleh Sysmex untuk pengguna alat Urinalisa otomatis Sysmex UN-series.
 </p>
 <p>
-File petunjuk pengerjaan material uji dan petunjuk unggah hasil dapat diunduh pada tautan <a href="{{url('storage/urin/Surat_Petunjuk_EQUAS_UFCM.pdf')}}">ini</a>.<br>
-Form hasil manual dapat diunduh pada tautan <a href="{{url('storage/urin/Form_Hasil_Manual_EQUAS_UFCM.pdf')}}">ini</a>.
+    Prosedur
 </p>
-<p>Terima kasih atas partisipasi Anda.</p>
-
-<p>Silakan mengisi hasil EQUAS Anda di menu <a href="{{route('urin.instrument')}}">MY EQUAS</a>.</p>
-
+<ol>
+    <li>Tanggal pelaksanaan EQUAS periode 2021 ini adalah pada tanggal 17-18 Februari 2022. </li>
+    <li>Material yang diterima disimpan di lemari pendingin pada suhu 2-10° C, tidak boleh disimpan dalam freezer.</li>
+    <li>
+        Persiapan material EQUAS dilakukan dengan mengeluarkan vial ”A” dan ”B” dari lemari pendingin. 
+        Biarkan selama 20 - 30 menit pada suhu ruang (10-30° C).
+    </li>
+    <li>Pastikan instrument (UF-4000 / UF-5000) dalam kondisi mode “Quality Control”.</li>
+    <li>Kocok material uji beberapa kali hingga tidak ada partikel yang menumpuk di dasar botol, kemudian kocok kuat – kuat hingga 20 kali.</li>
+    <li>Secara cepat (dalam 10 detik) setelah pengocokan, teteskan isi material uji ke dalam sampel cup sebanyak 0.6 mL (± 12 - 15 tetes).</li>
+    <li>
+        Secara cepat (dalam 10 detik), Letakkan cup sampel tersebut ke dalam STAT UNIT dengan bantuan adaptor, 
+        lalu dorong STAT UNIT ke dalam alat dan tekan ANALYSIS START
+    </li>
+    <li>Hasil dapat dilihat di Monitor dan otomatis terkirim ke U-WAM</li>
+    <li>Buang cup uji setelah digunakan karena tidak diperbolehkan untuk uji secara duplo dari wadah yang sama.</li>
+    <li>Lakukan analisis sebanyak 6 kali berturut-turut. Laporkan hasil analisis ke 2 – 6.</li>
+    <li>Simpan kembali material di lemari pendingin pada suhu 2-8° C.</li>
+    <li>
+        Laporkan hasil analisis parameter dalam satuan kuantitatif (/uL) dan hasil analisis conductivity dalam satuan mS/cm 
+        pada menu MY EQUAS (mohon merujuk ke lembar Prosedur Unggah Hasil EQUAS).
+    </li>
+    <li>
+        Jika laboratorium tidak mempunyai akses internet, pelaporan hasil dilakukan dengan mengisi 
+        dan mengirimkan formulir yang terlampir ke 0811-976-200 (Sysmex Call Center)
+    </li>
+    <li>Pelaporan hasil dilakukan paling lambat hari Jumat, 25 Februari 2022. </li>
+</ol>
+<p>
+    Hasil evaluasi dapat diakses pada tautan <a href="http://sysmex-indonesia.com/equas/urin">http://sysmex-indonesia.com/equas/urin</a> maksimal hari Jumat, 11 Maret 2022. 
+    Apabila hasil penilaian tidak baik, silakan hubungi Sysmex Call Center untuk ditindaklanjut.
+</p> 
+<p>Sertifikat partisipasi untuk Periode Tahun 2021 dapat diakses melalui tautan di atas. </p>
+<p>
+    Apabila terdapat pertanyaan lebih lanjut, silakan menghubungi Sysmex Indonesia Call Center 021-30026999, 021-29023008, 
+    atau Whatsapp +62 811-976-200 dan Telegram @sinecce_bot atas nama Sysmex Indonesia Call Center.
+</p>
+<p>Demikian pemberitahuan ini kami sampaikan, partisipasi Anda sangat kami harapkan.</p>
 
 @endsection
