d77e876fe5c42c359e842f6c49f43361c04b7a92
Adhil Novandri <novandri.adhil@sysmex.co.id>
2022-02-18 07:05:20 +0700

    update home koagulasi

--- resources/views/koagulasi/home.blade.php
+++ resources/views/koagulasi/home.blade.php
@@ -5,10 +5,9 @@
 <p>Pelanggan Yth. </p>
 
 <p>
-    Kami mengundang Anda untuk berpartisipasi dalam Sysmex EQUAS Coagulation Periode Oktober 2021. 
+    Kami mengundang Anda untuk berpartisipasi dalam Sysmex EQUAS Coagulation Periode FY 2021-02. 
     EQUAS adalah sebuah kombinasi program IQC (Internal Quality Control), EQAS (External Quality Assessment Scheme) dan Servis Teknis, 
-    yang diselenggarakan oleh Sysmex untuk pengguna alat Koagulasi otomatis Sysmex CA-600 series dan CS-Series 
-    yang melakukan pemeriksaan PT, APTT, dan atau D-Dimer secara rutin.
+    yang diselenggarakan oleh Sysmex untuk pengguna alat Koagulasi otomatis Sysmex CA-600 series dan CS-Series. 
 </p>
 
 <p>Pada tahun 2021, EQUAS Coagulation akan dilaksanakan 2 kali</p>
@@ -22,15 +21,15 @@
         <td>22-23 Maret 2021</td>
     </tr>
     <tr>
-        <td>EQUAS Oktober 2021</td>
-        <td>26-27 Oktober 2021</td>
+        <td>EQUAS Februari 2022</td>
+        <td>21-22 Februari 2022</td>
     </tr>
 </table>
 <p>*Catatan: Jadwal bisa berubah sewaktu-waktu. Mohon memperhatikan pengumuman dari panitia</p>
 
 <h5>Prosedur</h5>
 <ol>
-    <li>Tanggal pelaksanaan EQUAS periode Maret 2021 ini adalah pada tanggal 26-27 Oktober 2021. </li>
+    <li>Tanggal pelaksanaan EQUAS periode FY 2021-02  ini adalah pada tanggal 21-22 Februari 2022. </li>
     <li>Material yang diterima disimpan di lemari pendingin pada suhu 2-8° C.</li>
     <li>Persiapan material EQUAS dilakukan dengan mengeluarkan vial A dan B (untuk D-Dimer), dan vial C dan D (untuk PT dan APTT) dari lemari pendingin. Biarkan selama 15 menit pada suhu ruang (18-25° C).</li>
     <li>Larutkan material A, B, C, dan D dengan akuabides masing-masing sebanyak 1.0 mL kemudian homogenisasikan perlahan untuk menghindari terjadinya busa.</li>
@@ -40,11 +39,11 @@
     <li>Simpan kembali material di lemari pendingin pada suhu 2-8° C.</li>
     <li>Laporkan hasil dalam unit mg/L FEU dan dOD untuk D-Dimer, dan second (detik) untuk PT dan APTT pada menu MY EQUAS (mohon merujuk ke lembar Prosedur Unggah Hasil EQUAS). </li>
     <li>Jika laboratorium tidak mempunyai akses internet, pelaporan hasil dilakukan dengan mengisi dan mengirimkan formulir yang terlampir ke 0811-976-200 (Sysmex Call Center)</li>
-    <li>Pelaporan hasil dilakukan <b>paling lambat hari Senin, 1 November 2021</b>.  </li>
+    <li>Pelaporan hasil dilakukan <b>paling lambat hari Jumat, 25 Februari 2022</b>.  </li>
 </ol>
-<p>Hasil evaluasi dapat diakses pada tautan <a href="http://sysmex-indonesia.com/equas/koagulasi">http://sysmex-indonesia.com/equas/koagulasi</a> maksimal  tanggal Senin, 15 November 2021.  Apabila hasil penilaian tidak baik, silakan  hubungi Sysmex Call Center untuk ditindaklanjut. </p>
+<p>Hasil evaluasi dapat diakses pada tautan <a href="http://sysmex-indonesia.com/equas/koagulasi">http://sysmex-indonesia.com/equas/koagulasi</a> maksimal  tanggal Senin, 14 Maret 2022.  Apabila hasil penilaian tidak baik, silakan  hubungi Sysmex Call Center untuk ditindaklanjut. </p>
 
-<p>Sertifikat partisipasi untuk Periode Tahun 2021 dapat diakses pada akhir tahun 2021 dan akan diberikan kepada peserta yang mengikuti program EQUAS Coagulation ini sebanyak 2 (dua) periode setiap tahunnya.</p>
+<p>Sertifikat partisipasi untuk Periode Tahun 2022 dapat diakses pada akhir tahun 2022 dan akan diberikan kepada peserta yang mengikuti program EQUAS Coagulation ini sebanyak 2 (dua) periode setiap tahunnya.</p>
 
 <p>Apabila terdapat pertanyaan lebih lanjut, silakan menghubungi Sysmex Call Center 021-30026999, 021-29023008, atau Whatsapp 0811-976-200.</p>
 
